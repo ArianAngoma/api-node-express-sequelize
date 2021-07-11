b102c2104e16acc8a53411a69183d6b39209826b
unknown <arian.angoma@tecsup.edu.pe>
2021-07-11 04:43:42 -0500

    Validators Upload files corrected

--- routes/uploads.js
+++ routes/uploads.js
@@ -3,7 +3,7 @@ const {check} = require('express-validator');
 
 const {uploadFiles, updateImg} = require('../controllers/uploads');
 const {validateFields, validateJWT, validateFileUpload} = require("../middlewares");
-const {userExistsById, isStateUserTrueById, tablesAllowed} = require('../helpers');
+const {userExistsById, isStateUserTrueById, existsProductById, tablesAllowed} = require('../helpers');
 
 const router = Router();
 
@@ -15,9 +15,9 @@ router.put('/:table/:id', [
     // validateJWT,
     validateFileUpload,
     check('id', 'Id es requerido').notEmpty(),
-    check('id').custom(userExistsById),
-    check('id').custom(isStateUserTrueById),
     check('table').custom(table => tablesAllowed(table, ['users', 'products'])),
+    check('id').if(check('table').equals('users')).custom(userExistsById).custom(isStateUserTrueById),
+    check('id').if(check('table').equals('products')).custom(existsProductById),
     validateFields
 ], updateImg);
 
