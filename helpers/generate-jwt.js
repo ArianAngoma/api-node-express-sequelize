148ad486c2a0940556462ac2ed1a5d17df2a087d
unknown <arian.angoma@tecsup.edu.pe>
2021-07-22 21:39:35 -0500

    Generate JWT

--- helpers/generate-jwt.js
+++ helpers/generate-jwt.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const generateJWT = (uid) => {
     return new Promise((resolve, reject) => {
         const payload = {uid};
+
+        // Generar JWT
         jwt.sign(payload, process.env.SECRET_OR_PRIVATE_KEY, {
             expiresIn: '4h'
         }, (err, token) => {
