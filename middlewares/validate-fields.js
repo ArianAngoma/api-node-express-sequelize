3900312606205a15079c7cd35722866890141e57
unknown <arian.angoma@tecsup.edu.pe>
2021-08-06 23:12:17 -0500

    Validate Errors

--- middlewares/validate-fields.js
+++ middlewares/validate-fields.js
@@ -1,6 +1,7 @@
 const {validationResult} = require("express-validator");
 
 const validateFields = (req, res, next) => {
+    // Valida si existe errores antes de ejecutar el controlador
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json(errors);
     next();
